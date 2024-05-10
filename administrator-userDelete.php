8ad4dfbc16db1f819dee1427a5da3c6df8ef3485
Nrharuka <2201169@s.asojuku.ac.jp>
2024-05-10 10:56:01 +0900

    ユーザー一覧画面PHP、CSS作成中

---
+++ administrator-userDelete.php
@@ -0,0 +1,14 @@
+<?php session_start(); ?>
+<?php require 'default/header.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ユーザー削除画面</title>
+</head>
+<body>
+    
+</body>
+</html>
