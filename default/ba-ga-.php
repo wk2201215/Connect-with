3b8eed12ba900f5abf203844cfa28c4d1c4ba3ad
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-05-16 10:30:53 +0900

    ba-ga-

---
+++ default/ba-ga-.php
@@ -0,0 +1,26 @@
+<div  class="open">
+  <span class="cls"></span>
+  <span>
+    <ul class="sub-menu ">
+      <li>
+        <a href="#" title="home">home</a>
+      </li>
+      <li>
+        <a href="#" title="Registered books">Registered books</a>
+      </li>
+      <li>
+        <a href="#" title="Category">Category</a>
+      </li>
+      <li>
+        <a href="#" title="Logout">Logout</a>
+      </li>
+    </ul>
+  </span>
+  <span class="cls"></span>
+</div>
+
+
+<div class="container">
+  <br>
+  <br>
+</div>
