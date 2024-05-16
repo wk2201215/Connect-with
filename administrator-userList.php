8326bf95016513fc90412d653743fd3a64c684a5
Hibio117 <2201181@s.asojuku.ac.jp>
2024-05-16 10:43:01 +0900

    a

--- administrator-userList.php
+++ administrator-userList.php
@@ -1,13 +1,3 @@
-<?php
-    const SERVER = 'mysql302.phy.lolipop.lan';
-    const DBNAME = 'LAA1517442';
-    const USER = 'LAA1517442';
-    const PASS = 'post0418';
-
-    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
-?>
-
-
 <?php session_start(); ?>
 <?php require 'default/header.php'; ?>
 
@@ -16,7 +6,6 @@
 <button class="searchbutton" type="submit" name="user_search" value="ユーザー検索">🔍</button>
 <button class="searchbutton" type="submit" name="hashtag" value="ハッシュタグ検索">＃</button>
 </div>
-
 
 <table class="table">
 <!-- <?php
@@ -32,16 +21,17 @@
 // テーブルのデータを定義
 $products = array(
     array("user name", "restoration", "delete"),
-    array(1, "", ""),
-    array(2, "", ""),
-    array(3, "", ""),
-    array(4, "", ""),
-    array(5, "", ""),
-    array(6, "", ""),
-    array(7, "", ""),
-    array(8, "", ""),
-    array(9, "", ""),
-    array(10, "", ""),
+    array("", "", "delete"),
+    array("", "", "delete"),
+    array("", "", "delete"),
+    array("", "", "delete"),
+    array("", "", "delete"),
+    array("", "", "delete"),
+    array("", "", "delete"),
+    array("", "", "delete"),
+    array("", "", "delete"),
+    array("", "", "delete"),
+    array("", "", "delete"),
 );
 
 // テーブルの開始タグを出力
