a6bc7466c4b3e199f6f8187fe136903d9c094c8d
Hibio117 <2201181@s.asojuku.ac.jp>
2024-05-16 10:09:04 +0900

    a

--- administrator-userDelete.php
+++ administrator-userDelete.php
@@ -8,37 +8,15 @@
 <button class="user_button" type="submit" name="garbage_can" src="./images/🗑️.png" value="ユーザー削除">🗑️</button>
 </div>
 
-<body>
 <?php
-// テーブルのデータを定義
-$products = array(
-    array("user name", "restoration", "delete"),
-    array(1, "", ""),
-    array(2, "", ""),
-    array(3, "", ""),
-    array(4, "", ""),
-    array(5, "", ""),
-    array(6, "", ""),
-    array(7, "", ""),
-    array(8, "", ""),
-    array(9, "", ""),
-    array(10, "", "")
-);
-// テーブルの開始タグを出力
-echo "<table border='1'>";
-
-// テーブルの各行を出力
-foreach ($products as $row) {
-    echo "<tr>";
-    foreach ($row as $cell) {
-        echo "<td>$cell</td>";
-    }
-    echo "</tr>";
-}
-
-// テーブルの終了タグを出力
-echo "</table>";
+    unset($_SESSION['account']);
+    $pdo=new PDO($connect, USER, PASS);
+    $sql=$pdo->prepare('select * from account where mail-address=?');
+    $sql->execute([$_POST['mail-address']]);
 ?>
-</body>
+
+<input type="image" name="garbage_can" src="./images/🗑️.png"  alt="ユーザー削除" value="ユーザー削除">
+
+<?php require 'default/footer.php'; ?>
 
 
