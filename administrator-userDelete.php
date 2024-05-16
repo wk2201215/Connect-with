2a271bdf8027332f15461287f0764d87f1741f91
Nrharuka <2201169@s.asojuku.ac.jp>
2024-05-16 10:56:27 +0900

    Update administrator-userDelete.php

--- administrator-userDelete.php
+++ administrator-userDelete.php
@@ -1,3 +1,4 @@
+<?php require 'db/db-connect.php';?>
 <?php session_start(); ?>
 <?php require 'default/header.php'; ?>
 
@@ -5,16 +6,33 @@
 <input class="keyword" type="text" name="keyword" placeholder="  キーワード検索  ">
 <button class="user_button" type="submit" name="user_search" value="ユーザー検索">🔍</button>
 <button class="user_button" type="submit" name="hashtag" value="ハッシュタグ検索">＃</button>
-<button class="user_button" type="submit" name="garbage_can" src="./images/🗑️.png" value="ユーザー削除">🗑️</button>
+<!-- <button class="user_button" type="submit" name="garbage_can" src="./images/🗑️.png" value="ユーザー削除">🗑️</button> -->
 </div>
 
 <?php
     unset($_SESSION['account']);
     $pdo=new PDO($connect, USER, PASS);
-    $sql=$pdo->prepare('select * from account where mail-address=?');
-    $sql->execute([$_POST['mail-address']]);
+    $sql=$pdo->prepare('select * from account where account_name=?');
+    $sql->execute([$_POST['account_name']]);
+    foreach($sql as $row) {
+
+        var_dump($row);
+        exit;
+        // if($_POST['id'],$row['account_id']){
+        //     $_SESSION['account'] = [
+        //         'account_name'=>$row['account_name'],
+        //         'mail_address'=>$row['mail_address']
+        //     ];
+        
+        // }
+
+    }
+    // echo '<label><p1 style="font-size:20px;"></p1></label>name<br>';
+    // echo 
+    // echo '<label><p1 style="font-size:20px;"></p1></label>email address<br>';
+    // echo 
 ?>
 
-<input type="image" name="garbage_can" src="./images/🗑️.png"  alt="ユーザー削除" value="ユーザー削除">
+<button class="user_button" type="submit" name="garbage_can" value="ユーザー削除">🗑️</button>
 
 <?php require 'default/footer.php'; ?>
