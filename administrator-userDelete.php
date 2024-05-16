928ae5046c372bcc69809fefbbed6bb7a8b58b65
Hibio117 <2201181@s.asojuku.ac.jp>
2024-05-16 09:47:04 +0900

    a

--- administrator-userDelete.php
+++ administrator-userDelete.php
@@ -8,6 +8,39 @@
 <button class="user_button" type="submit" name="garbage_can" src="./images/🗑️.png" value="ユーザー削除">🗑️</button>
 </div>
 
+<body>
+<?php
+// テーブルのデータを定義
+$products = array(
+    array("Post", "User Name", "Delete"),
+    array(1, "", ""),
+    array(2, "", ""),
+    array(3, "", ""),
+    array(4, "", ""),
+    array(5, "", ""),
+    array(6, "", ""),
+    array(7, "", ""),
+    array(8, "", ""),
+    array(9, "", ""),
+    array(10, "", ""),
+);
+// テーブルの開始タグを出力
+echo "<table border='1'>";
+
+// テーブルの各行を出力
+foreach ($products as $row) {
+    echo "<tr>";
+    foreach ($row as $cell) {
+        echo "<td>$cell</td>";
+    }
+    echo "</tr>";
+}
+
+// テーブルの終了タグを出力
+echo "</table>";
+?>
+</body>
+
 <?php
     unset($_SESSION['account']);
     $pdo=new PDO($connect, USER, PASS);
