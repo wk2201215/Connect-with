077724c51558b02a69fcf4b20da1a09925550041
Hibio117 <2201181@s.asojuku.ac.jp>
2024-05-16 10:15:10 +0900

    a

--- administrator-userList.php
+++ administrator-userList.php
@@ -15,6 +15,37 @@
     }
 ?> -->
 </table>
-
-
 <?php require 'default/footer.php'; ?>
+
+<body>
+<?php
+// テーブルのデータを定義
+$products = array(
+    array("user name", "restoration", "delete"),
+    array(1, "", ""),
+    array(2, "", ""),
+    array(3, "", ""),
+    array(4, "", ""),
+    array(5, "", ""),
+    array(6, "", ""),
+    array(7, "", ""),
+    array(8, "", ""),
+    array(9, "", ""),
+    array(10, "", ""),
+);
+// テーブルの開始タグを出力
+echo "<table border='1'>";
+
+// テーブルの各行を出力
+foreach ($products as $row) {
+    echo "<tr>";
+    foreach ($row as $cell) {
+        echo "<td>$cell</td>";
+    }
+    echo "</tr>";
+}
+
+// テーブルの終了タグを出力
+echo "</table>";
+?>
+</body>
