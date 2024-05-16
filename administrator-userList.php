b2f925d412b775cf386f4cd0a46ccfa97f62e21f
Hibio117 <2201181@s.asojuku.ac.jp>
2024-05-16 10:57:13 +0900

    a

--- administrator-userList.php
+++ administrator-userList.php
@@ -18,8 +18,27 @@
 
 <body>
 <?php
+<?php
+unset($_SESSION['account']);
+$pdo=new PDO($connect, USER, PASS);
+$sql=$pdo->prepare('select * from account where mail_address=?');
+$sql->execute([$_POST['mail_address']]);
+foreach($sql as $row) {
+    if(password_verify($_POST['password'],$row['account_password'])){
+        $_SESSION['account']=[
+            'account_id'=>$row['account_id'],
+            'mail_address'=>$row['mail_address'],
+            'account_password'=>$_POST['account_password'],
+            'account_name'=>$row['account_name'],
+            'photograph_id'=>$row['photograph_id'],
+            'self-introduction'=>$row['self-introduction'],
+            'authority'=>$row['authority'],
+            'delete_flag'=>$row['delete_flag']
+        ];
+    }
+}
 // テーブルのデータを定義
-$products = array(
+$account = array(
     array("user name", "restoration", "delete"),
     array("", "", "delete"),
     array("", "", "delete"),
@@ -38,7 +57,7 @@ $products = array(
 echo "<table border='1'>";
 
 // テーブルの各行を出力
-foreach ($products as $row) {
+foreach ($account as $row) {
     echo "<tr>";
     foreach ($row as $cell) {
         echo "<td>$cell</td>";
