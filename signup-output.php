04d310344b5cb6ea681f62e9c499dd6dafaf8c0d
odahayato112 <hayato0112.0@icloud.com>
2024-05-17 10:18:10 +0900

    あ

--- signup-output.php
+++ signup-output.php
@@ -1,26 +1,26 @@
 <?php
 session_start();
-
+ 
 require 'db/db-connect.php';
 require 'default/header.php';
-
+ 
 try {
     $pdo = new PDO($connect, USER, PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->exec('SET foreign_key_checks = 0');
-
+ 
     // 入力されたメールアドレスが既に存在するかチェック
     $sql_check = $pdo->prepare('SELECT * FROM account WHERE mail_address = ?');
     $sql_check->execute([$_POST['mail_address']]);
     $existing_account = $sql_check->fetch(PDO::FETCH_ASSOC);
-
+ 
     if (!$existing_account) {
         if (isset($_SESSION['account'])) {
             $id = $_SESSION['account']['account_id'];
-
+ 
             // パスワードをハッシュ化
             $hashed_password = password_hash($_POST['account_password'], PASSWORD_DEFAULT);
-
+ 
             $sql = $pdo->prepare('UPDATE account SET account_name=?, mail_address=?, account_password=? WHERE id=?');
             $sql->execute([
                 $_POST['account_name'], $_POST['mail_address'], $hashed_password, $id
@@ -35,12 +35,12 @@ try {
         } else {
             // パスワードをハッシュ化
             $hashed_password = password_hash($_POST['account_password'], PASSWORD_DEFAULT);
-
+ 
             $sql = $pdo->prepare('INSERT INTO account (account_name, mail_address, account_password) VALUES (?, ?, ?)');
             $sql->execute([
                 $_POST['account_name'], $_POST['mail_address'], $hashed_password
             ]);
-
+ 
             echo 'お客様情報を登録しました。';
         }
     } else {
@@ -49,6 +49,6 @@ try {
 } catch (PDOException $e) {
     echo "エラー: " . $e->getMessage();
 }
-
+ 
 require 'default/footer.php';
 ?>
