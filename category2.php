940a19d0af8e31fddd768f05d08d202813c4ee3f
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-05-20 11:00:14 +0900

    category

--- category2.php
+++ category2.php
@@ -1,8 +1,14 @@
+<?php require 'db/db-connect.php'; ?>
 <?php require 'default/header-top.php'; ?>
 <?php require 'default/header-menu.php'; ?>
 
 <?php
-
+$pdo=new PDO($connect,USER,PASS);
+$sql=$pdo->query('select * from category');
+foreach($sql as $row){
+    echo $row['category_name'];
+    echo '<br>';
+}
 ?>
 
 <?php require 'default/footer-menu.php'; ?>
