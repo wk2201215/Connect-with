48107cdbb7d4403c6b021d91b66fea4e6c667a1d
odahayato112 <hayato0112.0@icloud.com>
2024-05-20 10:40:21 +0900

    footer

--- default/footer-menu.php
+++ default/footer-menu.php
@@ -3,7 +3,7 @@
       <ul id="nav">
         <li><a href="#">投稿</a></li>
         <li><a href="#">カテゴリー</a></li>
-        <li><a href="#">マイページ</a></li>
+        <li><a href="mypage.php">マイページ</a></li>
         <li><a href="#">チャット</a></li>
       </ul>
     </div>
