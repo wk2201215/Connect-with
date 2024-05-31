b344a7bba643d978d37e1ccb5972e86197fe452f
Hibio117 <2201181@s.asojuku.ac.jp>
2024-05-31 10:19:10 +0900

    a

--- View.php
+++ View.php
@@ -1,5 +1,6 @@
 <?php require 'db/db-connect.php'; ?>
 <?php session_start(); ?>
+<link rel="stylesheet" href="css/login.css">
 <?php require 'default/header-top.php'; ?>
 <?php require 'default/header-menu.php'; ?>
 <?php
