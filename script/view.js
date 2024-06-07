5f6dab4605ba31610aa677d1d478896a98786170
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-06-07 09:40:48 +0900

    aa

--- script/view.js
+++ script/view.js
@@ -15,13 +15,4 @@ $(document).ready(function () {
         var post_id = $(this).data('id');
         location.href='./post-reply.php?post_id='+post_id;
     });
-
-    $(".good").on("click", function() {
-        var post_id = $(this).data('id');
-        $.post('_ajax.php',{
-            post_id: post_id
-          },function(res){
-          /*3の処理*/
-          });
-    });
 })
