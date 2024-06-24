69e17be620b06faf335506748e40cef5a08c7d84
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-06-24 10:22:14 +0900

    a

--- hello.py
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/python3.7
-# coding:utf-8
-
-print ("Hello world!")
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cd /home/hogehoge/9/hogehoge/web/
-/usr/local/bin/pythonhoge /home/hoge/9/hogehoge/web/hello.py
-
-exit
