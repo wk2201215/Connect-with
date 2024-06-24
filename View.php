30fe178418cb5b6610c898e1e0f8b5e14d0569de
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-06-24 10:25:42 +0900

    a

--- View.php
+++ View.php
@@ -5,7 +5,9 @@
 <?php require 'default/header-menu.php'; ?>
 <?php
 $pdo=new PDO($connect,USER,PASS);
-$sql=$pdo->query('SELECT * FROM post INNER JOIN category ON post.category_id = category.category_id WHERE delete_flag=0 AND reply_id IS NULL');
+// query
+$sql=$pdo->prepare('SELECT * FROM post INNER JOIN category ON post.category_id = category.category_id WHERE delete_flag=0 AND reply_id IS NULL AND post.category_id=?');
+$sql->execute([$_SESSION['account']['category_id']]);
 echo '<div id="container">';
 require 'default/post.php';
 echo '</div>';
