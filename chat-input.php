79fde2185720e91c0ba167b5988cb2aefb7160b6
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-07-08 11:17:06 +0900

    a

--- chat-input.php
+++ chat-input.php
@@ -6,7 +6,7 @@
 echo '<div id="container">';
 echo 'チャットルーム追加(招待)';
 echo '<HR>';
-echo '<form action="chat-output.php" method="post">';
+echo '<form action="chat-output.php" method="post"  enctype="multipart/form-data">';
   if(isset($_GET['message'])){
     echo $_GET['message'];
   }
@@ -18,6 +18,8 @@ echo '<form action="chat-output.php" method="post">';
     echo 'ルーム名';
     echo '<input type="text" name="chatroom_name" required>';
     echo '<input type="hidden" name="to" value="two">';
+    echo '<div id="pura">増やす</div>';
+    echo '<div id="mai">減らす</div>';
   }
   echo '<br>';
   echo 'ルーム写真';
@@ -29,8 +31,6 @@ echo '<form action="chat-output.php" method="post">';
       echo '<br>';
     echo '</div>';
   echo '</div>';
-  echo '<div id="pura">増やす</div><br>';
-  echo '<div id="mai">減らす</div><br>';
   echo '<button type="submit">招待</button>';
 echo '</form>';
 echo '</div>';
--- chat-output.php
+++ chat-output.php
@@ -2,22 +2,46 @@
 <?php require 'db/db-connect.php'; ?>
 <?php
 $pdo=new PDO($connect,USER,PASS);
-foreach($_POST['mail'] as $row){
-    $sql=$pdo->prepare('SELECT * FROM account WHERE mail_address = ?');
-    $sql->execute([$row]);
-    $item=$sql->fetchAll();
+//imag
+$image = uniqid(mt_rand(), true);//ファイル名をユニーク化
+$image .= '.' . substr(strrchr($_FILES['image']['name'], '.'), 1);//アップロードされたファイルの拡張子を取得
+$file = "images/$image";
+$sql2 = "INSERT INTO photograph(photograph_path) VALUES (:image)";
+$stmt = $pdo->prepare($sql2);
+$stmt->bindValue(':image', $image, PDO::PARAM_STR);
+//初期アイコン
+$item2 = 0;
+if (!empty($_FILES['image']['name'])) {//ファイルが選択されていれば$imageにファイル名を代入
+move_uploaded_file($_FILES['image']['tmp_name'], './images/' . $image);//imagesディレクトリにファイル保存
+    if (exif_imagetype($file)) {//画像ファイルかのチェック
+        $message = '画像をアップロードしました';
+        $stmt->execute();
+        $sql3=$pdo->query('SELECT photograph_id FROM photograph ORDER BY photograph_id DESC
+        LIMIT 1');
+        $item2=$sql3->fetch();
+    } else {
+        $message = '画像ファイルではありません';
+        header('Location:chat-input.php');
+        exit();
+    } 
 }
-// $sql=$pdo->prepare('SELECT * FROM account WHERE mail_address = ?');
-// $sql->execute([$_POST['mail_address']]);
-// $item=$sql->fetch();
 
+// foreach($_POST['mail'] as $row){
+//     $sql=$pdo->prepare('SELECT * FROM account WHERE mail_address = ?');
+//     $sql->execute([$row]);
+//     $item=$sql->fetchAll();
+// }
 if($_POST['to']==='on'){
+    $sql=$pdo->prepare('SELECT * FROM account WHERE mail_address = ?');
+    $sql->execute([$_POST['mail'][0]]);
+    $item=$sql->fetch();
+
     $sql_check=$pdo->prepare('SELECT * FROM chatmember INNER JOIN chatroom ON chatmember.chatroom_id = chatroom.chatroom_id WHERE account_id = ? AND one_on_one = ? LIMIT 1');
-    $sql_check->execute([$_SESSION['account']['account_id'],$item[0]['account_id']]);
+    $sql_check->execute([$_SESSION['account']['account_id'],$item['account_id']]);
     $resultCount = $sql_check->rowCount();
 
     $sql_check2=$pdo->prepare('SELECT * FROM chatmember_invitation INNER JOIN chatroom ON chatmember_invitation.chatroom_id = chatroom.chatroom_id WHERE account_id = ? AND one_on_one = ? LIMIT 1');
-    $sql_check2->execute([$_SESSION['account']['account_id'],$item[0]['account_id']]);
+    $sql_check2->execute([$_SESSION['account']['account_id'],$item['account_id']]);
     $resultCount2 = $sql_check2->rowCount();
 
     if($resultCount == 1 || $resultCount2 == 1){
@@ -25,15 +49,15 @@ if($_POST['to']==='on'){
         header('Location:chat-input.php?to=1&message='.$message);
         exit();
     }
-    $sql_input=$pdo->prepare('INSERT INTO chatroom (chatroom_name1, chatroom_name2, number_people, one_on_one) VALUES (?, ?, ?, ?)');
-    if($_SESSION['account']['account_id'] < $item[0]['account_id']){
-        $sql_input->execute([$_SESSION['account']['account_name'], $item[0]['account_name'], 2, $_SESSION['account']['account_id']]);
+    $sql_input=$pdo->prepare('INSERT INTO chatroom (chatroom_name1, chatroom_name2, number_people, photograph_id, one_on_one) VALUES (?, ?, ?, ?, ?)');
+    if($_SESSION['account']['account_id'] < $item['account_id']){
+        $sql_input->execute([$_SESSION['account']['account_name'], $item['account_name'], 2, $item2['photograph_id'], $_SESSION['account']['account_id']]);
     }else{
-        $sql_input->execute([$item[0]['account_name'], $_SESSION['account']['account_name'], 2, $_SESSION['account']['account_id']]);
+        $sql_input->execute([$item['account_name'], $_SESSION['account']['account_name'], 2, $item2['photograph_id'], $_SESSION['account']['account_id']]);
     }
 }else{
-    $sql_input=$pdo->prepare('INSERT INTO chatroom (chatroom_name1, chatroom_name2, number_people, one_on_one) VALUES (?, ?, ?, DEFAULT)');
-    $sql_input->execute([$_POST['chatroom_name'], $_SESSION['account']['account_id'], 2]);
+    $sql_input=$pdo->prepare('INSERT INTO chatroom (chatroom_name1, chatroom_name2, number_people, photograph_id, one_on_one) VALUES (?, ?, ?, ?, DEFAULT)');
+    $sql_input->execute([$_POST['chatroom_name'], $_SESSION['account']['account_id'], count($_POST['mail']), $item2['photograph_id']]);
 }
 
 $inserted_id = $pdo->lastInsertId();
@@ -45,9 +69,12 @@ $sql_input3->execute([$inserted_id, $_SESSION['account']['account_id']]);
 //     $item['account_id']
 // ];
 //memberテーブル
-foreach($item as $row){
+foreach($_POST['mail'] as $row){
+    $sql_id=$pdo->prepare('SELECT * FROM account WHERE mail_address = ?');
+    $sql_id->execute([$row]);
+    $item3=$sql_id->fetch();
     $sql_input2=$pdo->prepare('INSERT INTO chatmember_invitation  (chatroom_id, account_id, invitation_id) VALUES (?, ?, ?)');
-    $sql_input2->execute([$inserted_id, $row['account_id'], $_SESSION['account']['account_id']]);
+    $sql_input2->execute([$inserted_id, $item3['account_id'], $_SESSION['account']['account_id']]);
 }
 header('Location:chat-top.php');
 exit();
