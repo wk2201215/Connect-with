51305c2af37a4da070e9a50ca0919c9ddfe150f7
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-07-17 11:16:38 +0900

    a

--- default/footer-menu-chat.php
+++ default/footer-menu-chat.php
@@ -1,12 +1,10 @@
 <footer>
     <div>
-      <ul id="nav">
         <form method="post" onsubmit="writeMessage(); return false;">
           <div id="post">
             <input id="message" name="message" type="text" value="" />
             <input type="button" value="送信" onclick="writeMessage()">
           </div>
         </form>
-      </ul>
     </div>
 </footer>
