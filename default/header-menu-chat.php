f0e7db78b69bb629a74b83ee3fa90462d4559534
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-07-19 01:47:37 +0900

    a

--- default/header-menu-chat.php
+++ default/header-menu-chat.php
@@ -1,5 +1,21 @@
 <header>
 <div>
     chat
-</div>  
+    </div>
+    <div id="cs">
+        :
+    </div>
+    <div id="popup-wrapper">
+      <div id="popup-inside">
+        <div id="close">x</div>
+        <div id="theme">
+          <h2>テーマ変更</h2>
+          <button class="color" id="default"></button>
+          <button class="color" id="black"></button>
+          <button class="color" id="white"></button>
+          <button class="color" id="blue"></button>
+          <button class="color" id="red"></button>
+        </div>
+      </div>
+    </div>
 </header>
