24c3a03c102b3d72ff116df895afaea4277b17e5
wk2201215 <148303751+wk2201215@users.noreply.github.com>
2024-07-19 12:00:20 +0900

    a

--- script/chat-top.js
+++ script/chat-top.js
@@ -1,8 +1,3 @@
-// $(document).ready(function () {
-//     $("#cs").on("click", function() {
-//         location.href='./chat-input.php?to=1';
-//     });
-// });
 //ポップアップ
 // const clickBtn = document.getElementById('click-btn');
 const clickBtn = document.getElementById('cs');
@@ -21,27 +16,50 @@ popupWrapper.addEventListener('click', e => {
   }
 });
 
-function theme(chatroom_id) {
+function theme(theme_id) {
     $.ajax({
-      url:'./dbconnect4.php', //送信先
+      url:'./chat-theme-db.php', //送信先
       type:'POST', //送信方法
       datatype: 'json', //受け取りデータの種類
       data:{
-        'chatroom_id' : chatroom_id,
-        'account_id' : account_id
+        'theme_id' : theme_id
       }
     })
     .then(
         function (data) {
-          location.reload();
+          theme2(data['body'],data['header'],data['moji']);
           console.log(data);
+          console.log("変更成功");
         },
         function () {
-            alert("削除失敗");
+            alert("変更失敗");
         }
     );
 }
+
 $(".color").on("click", function() {
-    // var count = $('div.mail').data('count');
-    theme();
+    var theme_id = $(this).data('id');
+    console.log(theme_id);
+    theme(theme_id);
+});
+
+let $b = $('body'),
+    $h = $('header'),
+    $f = $('footer'),
+    $n = $('#nav li a');
+function theme2(body_c, header_footer_c, n_c) {
+    $b.css('background-color',body_c);
+    $b.css('color',n_c);
+    $h.css('background-color',header_footer_c);
+    $h.css('color',n_c);
+    $f.css('background-color',header_footer_c);
+    $n.css('color',n_c);
+    console.log(body_c);
+    console.log("変更");
+}
+$(document).ready(function() {
+    var theme_id1 = $('#t').data('idb');
+    var theme_id2 = $('#t').data('idhf');
+    var theme_id3 = $('#t').data('idn');
+	theme2(theme_id1,theme_id2,theme_id3);
 });
